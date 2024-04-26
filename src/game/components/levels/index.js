8ea23e2655bfe6a5ec4a792e0971f14ee6fa3b83
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2024-04-26 02:54:01 +0200

    Add new zigzag level

--- src/game/components/levels/index.js
+++ src/game/components/levels/index.js
@@ -2,8 +2,9 @@ import level1 from "./initial-level.json";
 import level2 from "./showing-multiples-switches.json";
 import level3 from "./new-mecanic-with-vision-length.json";
 import level4 from "./level.002.json";
-import level5 from "./level.003.json";
-import level6 from "./level.004.json";
+import level5 from "./level.zigzag.json";
+import level6 from "./level.003.json";
+import level7 from "./level.004.json";
 
 export const index = [
   level1,
@@ -11,7 +12,8 @@ export const index = [
   level3,
   level4,
   level5,
-  level6
+  level6,
+  level7
 ];
 
 export default index;
